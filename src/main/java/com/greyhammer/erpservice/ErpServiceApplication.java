4312aa3c95813dffb1576d90c33850d1f5a68fe5
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2022-03-28 17:42:59 +0800

    Initial commit

---
+++ src/main/java/com/greyhammer/erpservice/ErpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.greyhammer.erpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpServiceApplication.class, args);
+    }
+
+}
